37b471f08715fd43016df94a899ea4af06780f0c
Thomasdev28 <boulanger.cap@hotmail.fr>
2021-06-10 14:38:14 +0200

    recup modif

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const mainRouter = require('express').Router();
 const agriculteursRoutes = require('./aggriRoutes');
+const concessionnairesRoutes = require('./concessionnaires.routes');
 
 mainRouter.use('/concessionnaires', concessionnairesRoutes);
+mainRouter.use('/aggriculture', agriculteursRoutes);
 
 module.exports = mainRouter;
