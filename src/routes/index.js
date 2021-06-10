40e5ac582e6d55eb80af49c5ec313e70cdea233a
axel <chapellieraxel@gmail.com>
2021-06-10 16:46:08 +0200

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,8 @@
 const mainRouter = require('express').Router();
-<<<<<<< HEAD
-const ficheRouter = require('./fiche_technique.routes');
-
-mainRouter.use('/fiche_technique', ficheRouter);
-=======
 // const agriculteursRoutes = require('./aggriRoutes');
 const marquesRoutes = require('./marqueRoutes');
 
 // mainRouter.use('/concessionnaires', concessionnairesRoutes);
 mainRouter.use('/marques', marquesRoutes);
->>>>>>> dev
 
 module.exports = mainRouter;
