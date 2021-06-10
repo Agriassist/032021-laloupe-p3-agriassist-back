2e636dd8d3a72d34030291aab10f3bea4e4c34b2
francigny <claire-francigny@orange.fr>
2021-06-10 14:21:56 +0200

    Claire table marque faite et fonctionnelle

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const mainRouter = require('express').Router();
-const agriculteursRoutes = require('./aggriRoutes');
+// const agriculteursRoutes = require('./aggriRoutes');
+const marquesRoutes = require('./marqueRoutes');
 
-mainRouter.use('/concessionnaires', concessionnairesRoutes);
+// mainRouter.use('/concessionnaires', concessionnairesRoutes);
+mainRouter.use('/marques', marquesRoutes);
 
 module.exports = mainRouter;
