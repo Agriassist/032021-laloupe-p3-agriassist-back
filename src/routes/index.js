6f2260ceef8efd208c6b8dbe7bc65fc86f8d566c
Thomasdev28 <boulanger.cap@hotmail.fr>
2021-06-17 15:27:25 +0200

    correcgtion modele

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ const adminRoutes = require('./administrateurs.routes');
 const partenariatRoutes = require("./partenariat.routes");
 const materielRouter = require('./materielRoutes');
 const parkRouter = require('./parkRoutes');
+const marqueRoutes = require('./marqueRoutes');
+const ficheRouter = require('./fiche_technique.routes');
+const modeleRouter = require('./modeleRoutes');
 
 mainRouter.use('/concessionnaires', concessionnairesRoutes);
 mainRouter.use('/agriculteurs', agriculteursRoutes);
@@ -12,6 +15,9 @@ mainRouter.use('/administrateur', adminRoutes);
 mainRouter.use('/partenariats', partenariatRoutes);
 mainRouter.use('/materiels', materielRouter);
 mainRouter.use('/park' , parkRouter)
+mainRouter.use('/marque', marqueRoutes);
+mainRouter.use('/fiche_technique', ficheRouter);
+mainRouter.use('/modele', modeleRouter)
 
 // mainRouter.use('/fiche_technique', ficheRouter);
 
