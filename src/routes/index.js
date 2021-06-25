698e0d65d5056b78097668e570037307bc00ff8b
axel <chapellieraxel@gmail.com>
2021-06-25 12:03:22 +0200

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,23 @@
 const mainRouter = require('express').Router();
-const agriculteursRoutes = require('./agriRoutes');
-const concessionnairesRoutes = require('./concessionnaires.routes');
-const adminRoutes = require('./administrateurs.routes');
-const partenariatRoutes = require('./partenariat.routes');
 const materielRouter = require('./materielRoutes');
 const parkRouter = require('./parkRoutes');
 const marqueRoutes = require('./marqueRoutes');
 const ficheRouter = require('./fiche_technique.routes');
 const modeleRouter = require('./modeleRoutes');
 const carnetRouter = require('./carnet_entretien.routes');
+const usersRouter = require('./Users.routes');
 
-const loginRouter = require('./login.routes')
+const loginRouter = require('./login.routes');
 
-mainRouter.use('/concessionnaires', concessionnairesRoutes);
-mainRouter.use('/agriculteurs', agriculteursRoutes);
-mainRouter.use('/administrateur', adminRoutes);
-mainRouter.use('/partenariats', partenariatRoutes);
 mainRouter.use('/materiels', materielRouter);
 mainRouter.use('/park', parkRouter);
 mainRouter.use('/marque', marqueRoutes);
 mainRouter.use('/fiche_technique', ficheRouter);
-mainRouter.use('/login', loginRouter)
+mainRouter.use('/login', loginRouter);
+mainRouter.use('/users', usersRouter);
 
-mainRouter.use('/modele', modeleRouter)
-mainRouter.use('/carnet_entretien', carnetRouter)
+mainRouter.use('/modele', modeleRouter);
+mainRouter.use('/carnet_entretien', carnetRouter);
 
 // mainRouter.use('/fiche_technique', ficheRouter);
 
