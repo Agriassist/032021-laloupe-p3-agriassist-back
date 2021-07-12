bddf6dc0f7dd3c8c7f4602b2625dde0d8600b429
axel <chapellieraxel@gmail.com>
2021-07-12 13:52:20 +0200

    fin de deconnexion

--- src/routes/Users.routes.js
+++ src/routes/Users.routes.js
@@ -9,10 +9,10 @@ const {
   clearCookie,
 } = require('../services/jwt');
 
+userRouter.get('/logout', clearCookie);
 userRouter.get('/', authenticateAdminWithJsonWebToken, getAllUsers);
 userRouter.get('/:id', authenticateAdminWithJsonWebToken, getOneUserById);
 userRouter.get('/materiel/:id', authenticateAgriWithJsonWebToken, getManyMaterielById);
-userRouter.get('/logout', clearCookie);
 userRouter.post('/', createOneUser, getOneUserById);
 userRouter.put('/:id', updateOneUser, getOneUserById);
 userRouter.delete('/:id', deleteOneUser);
