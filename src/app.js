3e851332639e3eda7fb38a5f1fe48f1fce0ec9b3
axel <chapellieraxel@gmail.com>
2021-07-28 13:52:41 +0200

    change

--- src/app.js
+++ src/app.js
@@ -11,15 +11,14 @@ app.use(
     credentials: true,
   }),
 );
+app.use('/images_profil', express.static('public/images_profil'));
+app.use('/images_modele', express.static('public/images_modele'));
+app.use('/images_fichetech', express.static('public/images_fichetech'));
+app.use('/images_facture', express.static('public/images_facture'));
+app.use('/images_bon_travail', express.static('public/bon_travail'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.use('/images_profil', express.static('../public/images_profil'));
-app.use('/images_modele', express.static('../public/images_modele'));
-app.use('/images_fichetech', express.static('../public/images_fichetech'));
-app.use('/images_facture', express.static('../public/images_facture'));
-app.use('/images_bon_travail', express.static('../public/bon_travail'));
 
 app.get('/', (req, res) => {
   res.status(200).json({ foo: 'hello' });
